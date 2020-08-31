924a9c3ba188829237067734fd51de2c1d134c30
SouthDam <45695887+SouthDam@users.noreply.github.com>
2020-08-31 23:17:01 +0200

    Update length-analysis.py

--- length-analysis.py
+++ length-analysis.py
@@ -3,25 +3,26 @@ from QG import QueGenerator
 import wikipedia
 import pandas as pd
 
-names=["Italy","China"]
+names = wikipedia.random(1000)
+print("names generated")
 bart = Summarizer()
 qg = QueGenerator()
 our_length =pd.DataFrame(columns=('input_length','summary_length','question_generated_num','question_kept_num'))
-for name in names:
+for idx, name in enumerate(names):
     try:
         input_raw_text = wikipedia.summary(name)
         input_length = len(input_raw_text.split())
-        print(input_length)
         summary = bart.ForIntro(name)
         summary_length = len(summary.split())
-        print(summary_length)
         cards = qg.generate(summary,name)
         questions_generated_num = len(cards)
-        print(questions_generated_num)
         questions_kept = [card for card in cards if card["isGood"]]
         questions_kept_num = len(questions_kept)
-        print(questions_kept_num)
-        our_length=our_length.append(pd.DataFrame({"input_length":[input_length],"summary_length":[summary_length],"question_generated_num":[questions_generated_num],"question_kept_num":[questions_kept_num]}),ignore_index=True)
+        our_length.loc[idx] = {'input_length':input_length,'summary_length':summary_length,'question_generated_num':questions_generated_num,'question_kept_num':questions_kept_num}
     except:
         pass
-print(our_length)
+    if idx%10==0:
+        file_name="anaysis-"+str(int(idx/10)+1)+".csv"
+        our_length.to_csv(file_name,index=False)
+        print("finish batch"+str(int(idx/10)+1))
+        our_length =pd.DataFrame(columns=('input_length','summary_length','question_generated_num','question_kept_num'))
